40a6f723fb51fedccd6edda757cc13e971b79ff9
Oliver Sulc <1001@adc.itgymnazium.cz>
2022-12-15 15:41:43 +0100

    updated 2

---
+++ BinaryTree/BST.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BST
+    {
+        public Node root;
+        public void Insert(int input)
+        {
+            var newItem = new Node() { Value = input };
+
+            if (root == null) { root = newItem; }
+            else
+            {
+                var prev = root;
+                while (true)
+                {
+                    if (input > prev.Value && prev.right != null) { prev.right = newItem; prev.right = prev; continue; }
+                    else if(input > prev.Value && prev.right == null) { prev.right = newItem; break; }
+                    if (input < prev.Value) { prev.left = newItem; prev.left = prev; continue; }
+                    else if(input < prev.Value) { prev.left = newItem; break; }
+                }
+
+
+                if (input < root.Value) { root.left = newItem; }
+            }
+        }
+
+        public void Print()
+        {
+
+        }
+    }
+}
